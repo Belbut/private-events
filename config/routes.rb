4a422d4c3cf47db306e89bfedf99b2de6e509cf1
Belbut <80793911+Belbut@users.noreply.github.com>
2025-07-14 22:33:47 +0100

    feat: add user routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'event/index'
+  devise_for :users, controllers: {
+    registrations: 'user/registrations'
+  }
+  resources :users, only: %i[index show]
+  resources :event, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
