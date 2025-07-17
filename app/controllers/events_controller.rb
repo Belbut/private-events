edca53a324330d819349a843392bca3662970c95
Belbut <80793911+Belbut@users.noreply.github.com>
2025-07-17 13:15:27 +0100

    feat: add create new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,18 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    current_user.events_created.build(params(:event))
+  end
+
+  protected
+
+  def event_params
+    params.expect(event: %i[name date location])
+  end
 end
