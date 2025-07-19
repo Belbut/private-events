af669147480ff7f663a1c7716a7d2de56a6c129a
Belbut <80793911+Belbut@users.noreply.github.com>
2025-07-19 16:22:16 +0100

    feat: add invitation #create & #destroy actions

---
+++ app/controllers/invitations_controller.rb
@@ -0,0 +1,9 @@
+class InvitationsController < ApplicationController
+  def create; end
+
+  def destroy; end
+
+  protected
+
+  def invitation_params; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   }
   resources :users, only: %i[index show]
   resources :events, only: %i[index new create show]
+  resources :invitations, only: %i[create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
